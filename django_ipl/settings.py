a1a0a112d76da0dd39edfdb1a9a262dffed6986a
Shriya Hireholi <shriya.hireholi@mountblue.tech>
2020-09-04 20:42:53 +0530

    changes settings.py

--- django_ipl/settings.py
+++ django_ipl/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -77,11 +80,13 @@ WSGI_APPLICATION = 'django_ipl.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+username = os.getenv("username")
+password = os.getenv("username")
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'django_ipl',
-        'USER': 'postgres',
+        'NAME': username,
+        'USER': password,
         'PASSWORD': 'postgres',
         'HOST': '127.0.0.1',
         'PORT': '5432',
